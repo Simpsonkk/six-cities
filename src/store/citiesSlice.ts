b1d1248eef2f523f9f68bcab5c67fbcdc16c5123
Tsaturian Volodymyr <vova.zczc@gmail.com>
2022-12-19 14:56:24 +0200

    add types for asyncThunks

--- src/store/citiesSlice.ts
+++ src/store/citiesSlice.ts
@@ -1,16 +1,25 @@
+import { AppDispatch } from './../types/state.model';
+import { AxiosInstance } from 'axios';
 import { FavoriteRoom } from './../types/favorite-status.model';
 import { Review, NewReview } from './../types/reviews.model';
 import { RoomDescription } from './../types/room-card.model';
 import { errorHandler } from './../services/error-hadler';
-import { saveUserAvatarUrl, removeUserAvatarUrl } from './../services/user-avatar-url';
+import {
+  saveUserAvatarUrl,
+  removeUserAvatarUrl,
+} from './../services/user-avatar-url';
 import { saveUserEmail, removeUserEmail } from './../services/user-email';
 import { saveToken, removeToken } from './../services/token';
 import { UserData } from './../types/user-data.model';
 import { AuthData } from '../types/auth-data.model';
-import { createAction, createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import {
+  createAction,
+  createAsyncThunk,
+  createSlice,
+  PayloadAction,
+} from '@reduxjs/toolkit';
 import { AppRoute, APIRoute, AuthorizationStatus } from '../const';
 import { InitialState } from '../types/state.model';
-import { api, store } from './store';
 import { updateRoomsArray } from '../util';
 
 const initialCity = 'Amsterdam';
@@ -23,176 +32,296 @@ const initialState: InitialState = {
   favoriteRooms: [],
   currentRoom: null,
   nearbyRooms: [],
-  reviews: []
+  reviews: [],
 };
 
-export const setListRoomAction = createAsyncThunk(
-  'setListRoom',
-  async () => {
-    const {data} = await api.get<RoomDescription[]>(APIRoute.ListRooms);
-    store.dispatch(loadListRooms(data));
+export const setListRoomAction = createAsyncThunk<
+  void,
+  undefined,
+  {
+    extra: {
+      api: AxiosInstance;
+    };
+    dispatch: AppDispatch;
   }
-);
-
-export const setFavoriteRoomsAction = createAsyncThunk(
-  'setFavoriteRooms',
-  async () => {
-    try {
-      const {data} = await api.get<RoomDescription[]>(APIRoute.Favorite);
-      store.dispatch(loadFavoriteRooms(data));
-    } catch (error) {
-      errorHandler(error);
-    }
+>('setListRoom', async (_, { extra, dispatch }) => {
+  const { api } = extra;
+  try {
+    const { data } = await api.get<RoomDescription[]>(APIRoute.ListRooms);
+    dispatch(loadListRooms(data));
+  } catch (error) {
+    errorHandler(error);
   }
-);
+});
 
-export const changeFavoriteRoomAction = createAsyncThunk(
+export const setFavoriteRoomsAction = createAsyncThunk<
+  void,
+  undefined,
+  {
+    extra: {
+      api: AxiosInstance;
+    };
+    dispatch: AppDispatch;
+  }
+>('setFavoriteRooms', async (_, { extra, dispatch }) => {
+  const { api } = extra;
+  try {
+    const { data } = await api.get<RoomDescription[]>(APIRoute.Favorite);
+    dispatch(loadFavoriteRooms(data));
+  } catch (error) {
+    errorHandler(error);
+  }
+});
+
+export const changeFavoriteRoomAction = createAsyncThunk<
+  void,
+  FavoriteRoom,
+  {
+    extra: {
+      api: AxiosInstance;
+    };
+    dispatch: AppDispatch;
+  }
+>(
   'changeFavoriteRoom',
-  async ({roomId, newFavoriteStatus}: FavoriteRoom) => {
+  async ({ roomId, newFavoriteStatus }, { extra, dispatch }) => {
+    const { api } = extra;
     try {
-      const {data} = await api.post<RoomDescription>
-      (`${APIRoute.Favorite}/${roomId}/${newFavoriteStatus}`);
-      store.dispatch(updateRooms(data));
-      store.dispatch(loadCurrentRoom(data));
+      const { data } = await api.post<RoomDescription>(
+        `${APIRoute.Favorite}/${roomId}/${newFavoriteStatus}`
+      );
+      dispatch(updateRooms(data));
+      dispatch(loadCurrentRoom(data));
     } catch (error) {
       errorHandler(error);
     }
   }
 );
 
-export const loginAction = createAsyncThunk(
-  'login',
-  async ({login: email, password}: AuthData) => {
-    try {
-      const {data} = await api.post<UserData>(APIRoute.Login, {email, password});
-      saveToken(data.token);
-      store.dispatch(loadAuthorizationStatus(AuthorizationStatus.Auth));
-      saveUserEmail(data.email);
-      saveUserAvatarUrl(data.avatarUrl);
-      store.dispatch(redirectToRoute(AppRoute.Main));
-    } catch (error) {
-      errorHandler(error);
-      store.dispatch(loadAuthorizationStatus(AuthorizationStatus.NoAuth));
-    }
+export const loginAction = createAsyncThunk<
+  void,
+  AuthData,
+  {
+    extra: {
+      api: AxiosInstance;
+    };
+    dispatch: AppDispatch;
   }
-);
-
-export const logoutAction = createAsyncThunk(
-  'logout',
-  async () => {
-    try {
-      api.delete(APIRoute.Logout);
-      removeToken();
-      removeUserEmail();
-      removeUserAvatarUrl();
-      store.dispatch(loadAuthorizationStatus(AuthorizationStatus.NoAuth));
-    } catch (error) {
-      errorHandler(error);
-    }
+>('login', async ({ login: email, password }, { extra, dispatch }) => {
+  const { api } = extra;
+  try {
+    const { data } = await api.post<UserData>(APIRoute.Login, {
+      email,
+      password,
+    });
+    saveToken(data.token);
+    dispatch(loadAuthorizationStatus(AuthorizationStatus.Auth));
+    saveUserEmail(data.email);
+    saveUserAvatarUrl(data.avatarUrl);
+    dispatch(redirectToRoute(AppRoute.Main));
+  } catch (error) {
+    errorHandler(error);
+    dispatch(loadAuthorizationStatus(AuthorizationStatus.NoAuth));
   }
-);
+});
 
-export const checkAuthStatusAction = createAsyncThunk(
-  'checkAuthStatus',
-  async () => {
-    try {
-      await api.get(APIRoute.Login);
-      store.dispatch(loadAuthorizationStatus(AuthorizationStatus.Auth));
-    } catch {
-      store.dispatch(loadAuthorizationStatus(AuthorizationStatus.NoAuth));
-    }
-  },
-);
-
-export const setCurrentRoomAction = createAsyncThunk(
-  'setCurrentRoom',
-  async (roomId: string | undefined) => {
-    try {
-      const {data} = await api.get<RoomDescription>(`${APIRoute.ListRooms}/${roomId}`);
-      store.dispatch(loadCurrentRoom(data));
-    } catch (error) {
-      store.dispatch(redirectToRoute(AppRoute.NotFound));
-      errorHandler(error);
-    }
+export const logoutAction = createAsyncThunk<
+  void,
+  undefined,
+  {
+    extra: {
+      api: AxiosInstance;
+    };
+    dispatch: AppDispatch;
   }
-);
-
-export const setNearbyRoomsAction = createAsyncThunk(
-  'setNearbyRooms',
-  async (roomId: string | undefined) => {
-    try {
-      const {data} = await api.get<RoomDescription[]>
-      (`${APIRoute.ListRooms}/${roomId}${APIRoute.NearbyRooms}`);
-      store.dispatch(loadNearbyRooms(data));
-    } catch (error) {
-      errorHandler(error);
-    }
+>('logout', async (_, { extra, dispatch }) => {
+  const { api } = extra;
+  try {
+    api.delete(APIRoute.Logout);
+    removeToken();
+    removeUserEmail();
+    removeUserAvatarUrl();
+    dispatch(loadAuthorizationStatus(AuthorizationStatus.NoAuth));
+  } catch (error) {
+    errorHandler(error);
   }
-);
+});
 
-export const setReviewsAction = createAsyncThunk(
-  'setReviews',
-  async (roomId: string | undefined) => {
-    try {
-      const {data} = await api.get<Review[]>
-      (`${APIRoute.Reviews}/${roomId}`);
-      store.dispatch(loadReviews(data));
-    } catch (error) {
-      errorHandler(error);
-    }
+export const checkAuthStatusAction = createAsyncThunk<
+  void,
+  undefined,
+  {
+    extra: {
+      api: AxiosInstance;
+    };
+    dispatch: AppDispatch;
   }
-);
-
-export const addReviewAction = createAsyncThunk(
-  'addReview',
-  async ({comment, rating, roomId}: NewReview) => {
-    try {
-      const {data} = await api.post<Review[]>(`${APIRoute.Reviews}/${roomId}`, {comment, rating});
-      store.dispatch(loadReviews(data));
-    } catch (error) {
-      errorHandler(error);
-    }
+>('checkAuthStatus', async (_, { extra, dispatch }) => {
+  const { api } = extra;
+  try {
+    await api.get(APIRoute.Login);
+    dispatch(loadAuthorizationStatus(AuthorizationStatus.Auth));
+  } catch {
+    dispatch(loadAuthorizationStatus(AuthorizationStatus.NoAuth));
   }
-);
+});
 
-export const redirectToRoute = createAction('redirectToRoute', (value) => ({payload: value}));
+export const setCurrentRoomAction = createAsyncThunk<
+  void,
+  string | undefined,
+  {
+    extra: {
+      api: AxiosInstance;
+    };
+    dispatch: AppDispatch;
+  }
+>('setCurrentRoom', async (roomId, { extra, dispatch }) => {
+  const { api } = extra;
+  try {
+    const { data } = await api.get<RoomDescription>(
+      `${APIRoute.ListRooms}/${roomId}`
+    );
+    dispatch(loadCurrentRoom(data));
+  } catch (error) {
+    dispatch(redirectToRoute(AppRoute.NotFound));
+    errorHandler(error);
+  }
+});
+
+export const setNearbyRoomsAction = createAsyncThunk<
+  void,
+  string | undefined,
+  {
+    extra: {
+      api: AxiosInstance;
+    };
+    dispatch: AppDispatch;
+  }
+>('setNearbyRooms', async (roomId: string | undefined, { extra, dispatch }) => {
+  const { api } = extra;
+  try {
+    const { data } = await api.get<RoomDescription[]>(
+      `${APIRoute.ListRooms}/${roomId}${APIRoute.NearbyRooms}`
+    );
+    dispatch(loadNearbyRooms(data));
+  } catch (error) {
+    errorHandler(error);
+  }
+});
+
+export const setReviewsAction = createAsyncThunk<
+  void,
+  string | undefined,
+  {
+    extra: {
+      api: AxiosInstance;
+    };
+    dispatch: AppDispatch;
+  }
+>('setReviews', async (roomId: string | undefined, { extra, dispatch }) => {
+  const { api } = extra;
+  try {
+    const { data } = await api.get<Review[]>(`${APIRoute.Reviews}/${roomId}`);
+    dispatch(loadReviews(data));
+  } catch (error) {
+    errorHandler(error);
+  }
+});
+
+export const addReviewAction = createAsyncThunk<
+  void,
+  NewReview,
+  {
+    extra: {
+      api: AxiosInstance;
+    };
+    dispatch: AppDispatch;
+  }
+>('addReview', async ({ comment, rating, roomId }, { extra, dispatch }) => {
+  const { api } = extra;
+  try {
+    const { data } = await api.post<Review[]>(`${APIRoute.Reviews}/${roomId}`, {
+      comment,
+      rating,
+    });
+    dispatch(loadReviews(data));
+  } catch (error) {
+    errorHandler(error);
+  }
+});
+
+export const redirectToRoute = createAction('redirectToRoute', (value) => ({
+  payload: value,
+}));
 
 const citiesSlice = createSlice({
   name: 'cities',
   initialState,
   reducers: {
     changeCity: (state, action: PayloadAction<string>) => {
-      state.currrentCity = action.payload;},
+      state.currrentCity = action.payload;
+    },
     loadListRooms: (state, action: PayloadAction<RoomDescription[]>) => {
       state.roomList = action.payload;
-      state.isDataLoaded = true;},
+      state.isDataLoaded = true;
+    },
     loadFavoriteRooms: (state, action: PayloadAction<RoomDescription[]>) => {
-      state.favoriteRooms = action.payload;},
-    loadAuthorizationStatus: (state, action: PayloadAction<AuthorizationStatus>) => {
-      state.authorizationStatus = action.payload;},
+      state.favoriteRooms = action.payload;
+    },
+    loadAuthorizationStatus: (
+      state,
+      action: PayloadAction<AuthorizationStatus>
+    ) => {
+      state.authorizationStatus = action.payload;
+    },
     loadCurrentRoom: (state, action: PayloadAction<RoomDescription>) => {
-      state.currentRoom = action.payload;},
+      state.currentRoom = action.payload;
+    },
     loadNearbyRooms: (state, action: PayloadAction<RoomDescription[]>) => {
-      state.nearbyRooms = action.payload;},
+      state.nearbyRooms = action.payload;
+    },
     loadReviews: (state, action: PayloadAction<Review[]>) => {
-      state.reviews = action.payload;},
+      state.reviews = action.payload;
+    },
     updateRooms: (state, action: PayloadAction<RoomDescription>) => {
-      const nearbyRoomIndex = state.nearbyRooms.findIndex((room: RoomDescription) => room.id === action.payload.id);
+      const nearbyRoomIndex = state.nearbyRooms.findIndex(
+        (room: RoomDescription) => room.id === action.payload.id
+      );
 
       if (nearbyRoomIndex >= 0) {
-        state.nearbyRooms = updateRoomsArray(state.nearbyRooms, action.payload, nearbyRoomIndex);
+        state.nearbyRooms = updateRoomsArray(
+          state.nearbyRooms,
+          action.payload,
+          nearbyRoomIndex
+        );
       }
 
       if (!action.payload.isFavorite) {
-        state.favoriteRooms = state.favoriteRooms.filter((room) => room.id !== action.payload.id);
+        state.favoriteRooms = state.favoriteRooms.filter(
+          (room) => room.id !== action.payload.id
+        );
       }
 
-      const roomIndex = state.roomList.findIndex((room: RoomDescription) => room.id === action.payload.id);
-      state.roomList = updateRoomsArray(state.roomList, action.payload, roomIndex);
-    }
-  }
+      const roomIndex = state.roomList.findIndex(
+        (room: RoomDescription) => room.id === action.payload.id
+      );
+      state.roomList = updateRoomsArray(
+        state.roomList,
+        action.payload,
+        roomIndex
+      );
+    },
+  },
 });
 
-export const { changeCity, loadListRooms, loadFavoriteRooms,
-  loadAuthorizationStatus,loadCurrentRoom, loadNearbyRooms, loadReviews, updateRooms } = citiesSlice.actions;
+export const {
+  changeCity,
+  loadListRooms,
+  loadFavoriteRooms,
+  loadAuthorizationStatus,
+  loadCurrentRoom,
+  loadNearbyRooms,
+  loadReviews,
+  updateRooms,
+} = citiesSlice.actions;
 export default citiesSlice.reducer;
